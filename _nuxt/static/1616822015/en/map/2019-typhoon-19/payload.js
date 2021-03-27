b336be35ae6110c09269c13a01efd368d7aa5980
yuiseki <yuiseki@users.noreply.github.com>
2021-03-27 05:13:38 +0000

    deploy: 710ab7efdda2e20d8dc0e77354a301c651cfc799

---
+++ _nuxt/static/1616822015/en/map/2019-typhoon-19/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/en/map/2019-typhoon-19", {data:[{updated_at:"3\u002F27\u002F2021, 5:13:35 AM"}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1616822015/tw/map/2019-typhoon-19/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/tw/map/2019-typhoon-19", {data:[{updated_at:"2021年03月27日05時13分"}],fetch:{},mutations:void 0});
